b4993f1d6fc335af17555648638e3977b813471a
Juan Jose Garcia Sanchez <josejjgs1923@gmail.com>
2024-11-14 15:29:06 -0500

    agregar archivo stats.py, usado para herramientas especificas del analisis estadistico.

---
+++ stats.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+Almacenar funciones y clases que facilitan los EDAs, analisis de hipotesis, probabilidad y en general estadistica.
+"""
+
+if __name__ == "__main__":
+    pass
